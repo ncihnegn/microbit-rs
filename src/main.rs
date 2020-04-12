89077fc5bc274f3fbb6c68d0019815aab9d56015
ncihnegn <ncihnegn@users.noreply.github.com>
2020-04-12 01:07:15 -0700

    Use cortex_m_rt::entry

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,12 @@
 #![no_std]
 #![no_main]
 
-extern crate cortex_m_rt;
 extern crate microbit;
 extern crate panic_halt;
 
-#[no_mangle]
-pub extern "C" fn main() -> ! {
+use cortex_m_rt::entry;
+
+#[entry]
+fn main() -> ! {
     loop {}
 }
